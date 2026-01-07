8cca7f115b0824d168b6ac267d74ac5ffbed2dab
Arthur Toledo <toledoarthur11@gmil.com>
2026-01-07 19:44:29 -0300

    Primeiros passos app links

---
+++ React Native Aula/links/src/app/index/styles.ts
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet, TextInput} from 'react-native'
+
+import {colors} from '../../styles/colors'
+
+
+export const styles =  StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:  'center',
+  },
+  title: {
+    color: colors.green[300],
+    fontSize: 45,
+  }
+})
