13b2f062f2ed252efcc63056ea1dfe45b693c957
Arthur Toledo <toledoarthur11@gmil.com>
2026-01-08 19:03:07 -0300

    criando o componente CATEGORY

--- React Native Aula/links/src/app/index/index.tsx
+++ React Native Aula/links/src/app/index/index.tsx
@@ -4,15 +4,21 @@ import { MaterialIcons } from "@expo/vector-icons"
 import {styles} from './styles'
 import {colors} from '@/styles/colors'
 
+import { Category } from '@/components/category'
+
 export default function Index() {
   return (
     <View style = {styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.header}>
+
+     
     <Image source={require("@/assents/logo.png")} style={styles.logo}/>
 
       <TouchableOpacity>
      <MaterialIcons name="add" size={32} color={colors.green[300]} /> </TouchableOpacity>
+      </View>
 
+    <Category />
    </View>
 
   )
