bd966a35b5140e17bd9ccb5b760d25ae29f9af8b
Arthur Toledo <toledoarthur11@gmil.com>
2026-01-11 21:37:21 -0300

    Quarta aula React Native Alura

--- React Native aula Alura/fokus/app/index.jsx
+++ React Native aula Alura/fokus/app/index.jsx
@@ -1,13 +1,28 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.conatiner}
-    >
+    <View style={styles.conatiner}>
+
       <Image source={require('./foco.png')}/>
-      <View style={styles.actions}/>
-    </View>
+
+     <View style={styles.actions}>
+       <Text style={styles.timer}>
+      25:00
+      </Text>
+      <Pressable style={styles.buttom}>
+        <Text style={styles.buttomtext}>
+          Começar
+        </Text>
+      </Pressable>
+       </View> 
+       <View style={styles.footer}>
+        <Text style={styles.footerText}>Projeto fictício e sem fins comerciais.
+         </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura. 
+        </Text>
+       </View>
+     </View>
   );
 }
 
@@ -27,7 +42,32 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 32,
     borderWidth: 2,
-    borderColor: '#144480'
-
+    borderColor: '#144480',
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
+    color: '#ffff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttom: {
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8,
+    
+  },
+  buttomtext: {
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18,
+  },
+  footer: {
+    width: '80%',
+  },
+  footerText:{
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
   }
 })
