7a51a7cd0fb65266f91dc2436bf9a7a99b890873
Arthur-018 <toledoarthur11@gmail.com>
2026-01-15 10:27:37 -0300

    Add files via upload

--- React Native aula Alura/fokus/app/index.jsx
+++ React Native aula Alura/fokus/app/index.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View, Pressable } from "react-native";
-import { FokusButton } from '../components/FokusButton';
+import { FokusButton } from "../components/FokusButton";
 import { ActionButton } from "../components/ActionButton";
 import { Timer } from "../components/Timer";
+import { IconPause, IconPlay } from "../components/Icons";
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
+    initialValue: 25 * 60,
     image: require('./pomodoro.png'),
     display: 'Foco'
   },
   {
     id: 'short',
-    initialValue: 5,
+    initialValue: 5 * 60,
     image: require('./short.png'),
     display: 'Pausa curta'
   },
   {
     id: 'long',
-    initialValue: 15,
+    initialValue: 15 * 60,
     image: require('./long.png'),
     display: 'Pausa longa'
   },
@@ -28,18 +29,16 @@ const pomodoro = [
 export default function Index() {
 
   const [timerType, setTimerType] = useState(pomodoro[0])
-  const[seconds, setSeconds] = useState(pomodoro(0).initialValue)
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
   const [timerRunning, setTimerRunning] = useState(false)
-
-
 
   const timerRef = useRef(null)
 
   const clear = () => {
-    if(timerRef.current != null){
-    clearInterval(timerRef.current)
-    timerRef.current = null
-    setTimerRunning(false)
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current)
+      timerRef.current = null
+      setTimerRunning(false)
     }
   }
 
@@ -47,15 +46,14 @@ export default function Index() {
     setTimerType(newTimerType)
     setSeconds(newTimerType.initialValue)
     clear()
-
-  }
+  }  
 
   const toggleTimer = () => {
     if (timerRef.current) {
       clear()
       return
     }
-
+    
     setTimerRunning(true)
 
     const id = setInterval(() => {
@@ -64,10 +62,8 @@ export default function Index() {
           clear()
           return timerType.initialValue
         }
-        return oldState -1
+        return oldState - 1
       })
-      console.log('Timer rolando');
-
     }, 1000)
     timerRef.current = id
   }
@@ -76,29 +72,31 @@ export default function Index() {
     <View
       style={styles.container}
     >
-      <Image source={timerType.image} />
+      <Image source={timerType.image}/>
       <View style={styles.actions}>
         <View style={styles.context}>
           {pomodoro.map(p => (
-            <ActionButton
+            <ActionButton 
               key={p.id}
-              active={timerType.id === p.id}
+              active={ timerType.id === p.id }
               onPress={() => toggleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
         <Timer totalSeconds={seconds} />
-        <FokusButton
+        <FokusButton 
           title={timerRunning ? 'Pausar' : 'Começar'}
-          onPress={toggleTimer} />
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          onPress={toggleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
         </Text>
         <Text style={styles.footerText}>
-          Desenvolvido por Arthur!!!!!!!!!!.
+          Desenvolvido por Alura. 
         </Text>
       </View>
     </View>
