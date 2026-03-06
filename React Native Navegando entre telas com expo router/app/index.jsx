3c192cb0bac7c903602ac7d340cb08726d3bc16d
Arthur-018 <toledoarthur11@gmail.com>
2026-03-06 09:19:46 -0300

    Add files via upload

---
+++ React Native Navegando entre telas com expo router/app/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index () {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Link href={{ pathname: '/pomodoro' }}>
+        Quero iniciar!
+      </Link>
+    </View>
+  )
+}
