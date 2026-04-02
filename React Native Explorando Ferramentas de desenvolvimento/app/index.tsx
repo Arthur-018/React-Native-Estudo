0db05b3ca3c56946bf775bedc6bc499e51729175
Arthur-018 <toledoarthur11@gmail.com>
2026-04-02 09:30:25 -0300

    Add files via upload

--- React Native Explorando Ferramentas de desenvolvimento/app/index.tsx
+++ React Native Explorando Ferramentas de desenvolvimento/app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcon from "@expo/vector-icons/MaterialIcons"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <LinearGradient colors={["#00457D", "#05051F"]} style={styles.container}>
+      <Image source={require("../assets/images/logo.png")}/>
+      <Image source={require("../assets/images/weather.png")}/>
+
+      <Text style={styles.tittle}>Boas-vindas!</Text>
+
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonTittle}>Entrar</Text>
+      <MaterialIcon name="arrow-forward" size={24} color={"#01080E"}/>
+    </TouchableOpacity>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 64,
+    paddingVertical: 79,
+    paddingHorizontal: 32,
+  },
+  tittle: {
+    fontSize: 25,
+    color: "#FFF",
+  },
+  button: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#7693FF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 32,
+    flexDirection: "row",
+    gap: 8,
+  },
+  buttonTittle: {
+    color: "#01080E",
+    fontSize: 20,
+    fontWeight: "600",
+  }
+})
