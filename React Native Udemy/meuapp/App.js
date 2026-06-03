512468901e7af0c8dcfd6199ebf0cd79eb9f2980
André Arthur Toledo Ferreira <Andre.Ferreira@ciazaffari.com.br>
2026-06-03 09:35:42 -0300

    6 aula de React Native Udemy

--- React Native Udemy/meuapp/App.js
+++ React Native Udemy/meuapp/App.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 class App extends Component {
-
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={[styles.text1, styles.alinhaTexto]}>Eu sou texto 1</Text>
-        <Text style={styles.alinhaTexto}>Eu sou texto 2</Text>
-        <Text>Eu sou texto 3</Text>
-        <Text style={styles.text1}>Eu sou texto 4</Text>
+      <View style={{ flex: 1, backgroundColor: '#222' }}>
+
+        <View style={{ height: 65, backgroundColor: '#222' }}></View>
+
+        <View style={{ flex: 1, backgroundColor: '#FFF' }}></View>
+
+        <View style={{ height: 65, backgroundColor: '#222' }}></View>
+
 
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-  },
-  text1: {
-    fontSize: 25,
-    color: "#FF0000"
-  },
-  alinhaTexto: {
-    textAlign: "center"
-  }
-})
 
 export default App;
