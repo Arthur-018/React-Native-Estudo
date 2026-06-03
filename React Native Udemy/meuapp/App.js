84d6ca846375b2387767f10cad025a373e71e9b7
André Arthur Toledo Ferreira <Andre.Ferreira@ciazaffari.com.br>
2026-06-03 09:43:41 -0300

    7 aula de React Native Udemy

--- React Native Udemy/meuapp/App.js
+++ React Native Udemy/meuapp/App.js
@@ -5,13 +5,17 @@ import { Text, View } from 'react-native';
 class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#222' }}>
+      <View style={{
+        flex: 1, flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end'
+      }}>
 
-        <View style={{ height: 65, backgroundColor: '#222' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'green' }}></View>
 
-        <View style={{ flex: 1, backgroundColor: '#FFF' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'red' }}></View>
 
-        <View style={{ height: 65, backgroundColor: '#222' }}></View>
+        <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}></View>
 
 
       </View>
